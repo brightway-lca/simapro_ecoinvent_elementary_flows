7a8d4552e0d1018cac66303b914e87b7184211b0
Chris Mutel <cmutel@gmail.com>
2022-09-23 11:17:14 +0200

    Print errors to CI output

--- Scripts/raise_error.py
+++ Scripts/raise_error.py
@@ -1,5 +1,7 @@
+import json
 import sys
 from pathlib import Path
+from pprint import pprint
 
 
 BASE_DIR = Path(__file__).parent.parent.resolve()
@@ -8,6 +10,10 @@ LOGS_DIR = BASE_DIR / "Logs"
 
 def check_duplicates():
     if any(filename.suffix.lower() == ".json" for filename in LOGS_DIR.iterdir()):
+        print("Errors found:")
+        for filename in LOGS_DIR.iterdir():
+            pprint(json.load(open(filename)))
+
         sys.exit(1)
 
 
